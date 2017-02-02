2169897220cc31d8c26a573638b98e113d23e355
Nathan Rodman <nathanrodman@gmail.com>
2017-02-01 19:17:20 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+
+  render() {
+    return (
+      <a href='https://facebook.github.io/react/docs/thinking-in-react.html'>Thinking in React</a>
+    );
+  }
+}
+
+export default App;
