9ce7a77b0746fda998852b342fdcedd1a017240e
Nathan Rodman <nathanrodman@gmail.com>
2017-02-01 20:02:13 -0800

    Added a shit-ton of links

--- src/App.js
+++ src/App.js
@@ -10,7 +10,20 @@ class App extends Component {
       resources: [
         {title: "Thinking in React", url: 'https://facebook.github.io/react/docs/thinking-in-react.html'},
         {title: "ES6 var, let, or const", url:'https://medium.com/javascript-scene/javascript-es6-var-let-or-const-ba58b8dcde75#.xv9uppvae'},
-        {title: "ES6 interactive guide", url: 'http://stack.formidable.com/es6-interactive-guide/#/'}
+        {title: "ES6 interactive guide", url: 'http://stack.formidable.com/es6-interactive-guide/#/'},
+        {title: 'Higher Order Functions', url:'https://www.youtube.com/watch?v=BMUiFMZr7vk&list=PL0zVEGEvSaeEd9hlmCXrk5yUyqUag-n84'},
+        {title: 'var, let and const - What, why and how - ES6 JavaScript Features', url: 'https://www.youtube.com/watch?v=sjyJBL5fkp8'},
+        {title: 'Arrow functions in JavaScript - What, Why and How ', url: 'https://www.youtube.com/watch?v=6sQDTgOqh-I'},
+        {title: 'Essential ES6 / ES2015 JavaScript', url: 'https://www.youtube.com/watch?v=CozSF5abcTA'},
+        {title: 'Javascript ES6 Cheatsheet - the best of JS ES6', url: 'https://www.youtube.com/watch?v=AfWYO8t7ed4'},
+        {title: 'Javascript ES6 Cheatsheet #2 - the best of JS ES6', url: 'https://www.youtube.com/watch?v=LmL0Gh193M0'},
+        {title: 'ECMAScript 6 / ES6 New Features', url: 'https://www.youtube.com/watch?v=ZJZfIw3P8No&list=PL6gx4Cwl9DGBhgcpA8eTYYWg7im72LgLt'},
+        {title: 'Learn ES6 (ECMAScript 2015)', url: 'https://egghead.io/courses/learn-es6-ecmascript-2015'},
+        {title: 'ES6 JavaScript - The Basics', url: 'https://www.youtube.com/watch?v=IEf1KAcK6A8'},
+        {title: 'How it feels to learn JavaScript in 2016', url: 'https://hackernoon.com/how-it-feels-to-learn-javascript-in-2016-d3a717dd577f#.olyvd43o1'},
+        {title: 'ReactJS Basics - #1 What is React?', url: 'https://www.youtube.com/watch?v=JPT3bFIwJYA&list=PL55RiY5tL51oyA8euSROLjMFZbXaV7skS'},
+        {title: 'REACT JS TUTORIAL #2 - Reactjs Components & Rendering', url: 'https://www.youtube.com/watch?v=fd2Cayhez58&feature=youtu.be'},
+        
       ]
     };
   }
